08873477dd3157bd2620e1cf8ebfd7b7dc2c2281
BECOMEX\janderson.souza <janderson.souza@becomex.com.br>
2024-11-07 19:38:56 -0300

    feat: add authentication service with clerk

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import { redirect } from "next/navigation";
 
-const Login = () => {
+const Login = async () => {
+  const { userId } = await auth();
+
+  if (userId) redirect("/");
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -14,10 +20,12 @@ const Login = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       <div className="relative h-full w-full">
         <Image src="/login.png" alt="login" fill className="object-cover" />
